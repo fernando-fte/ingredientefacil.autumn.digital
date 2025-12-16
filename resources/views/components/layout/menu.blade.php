acc29743de9277c3844a5fcb1f279482d0b24880
Fernando Truculo Evangelista <fernando.fte@gmail.com>
2025-12-16 00:08:13 -0300

    feat: reorganiza o menu de navegação e adiciona componente central para receitas

--- resources/views/components/layout/menu.blade.php
+++ resources/views/components/layout/menu.blade.php
@@ -3,10 +3,11 @@
         <a class="navbar-brand" href="/">
             <i class="fa fa-utensils me-2"></i> {{ config('app.name', 'Laravel') }}
         </a>
-        <div class="d-flex align-items-center ms-auto">
-            @auth
+        <div class="collapse navbar-collapse justify-content-center">
+            <x-layout.menu.center />
+            <div class="d-flex align-items-center position-absolute end-0 pe-3">
                 <x-profile.dropdown />
-            @endauth
+            </div>
         </div>
     </div>
 </nav>
